7692961dd3c14aba0e0a5b93342e2a730e3288e4
Eugen <epopescuu@yahoo.it>
2024-02-01 20:43:20 +0100

    feat(category): show

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -3,16 +3,13 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h2 class="py-3 text-center">{{ $portfolio->title }}</h2>
-            <div class="col-4">
-                <img style="width: 100%" class="" src="{{ $portfolio->img }}" alt="{{ $portfolio->title }}">
-            </div>
-            <div class="col-8">
-                <p>{{ $portfolio->description }}</p>
+            <h2 class="py-3 text-center">{{ $category->name }}</h2>
+            <div class="col-12">
+                <p>{{ $category->description }}</p>
             </div>
             <div class="py-3 text-center">
-                <a href="{{ route('admin.portfolios.index') }}" class="btn btn-primary">Torna alla projects list</a>
-                <a href="{{ route('admin.portfolios.edit', $portfolio->id) }}" class="btn btn-warning">Modifica</a>
+                <a href="{{ route('admin.categories.index') }}" class="btn btn-primary">Torna alla projects list</a>
+                <a href="{{ route('admin.categories.edit', $category->id) }}" class="btn btn-warning">Modifica</a>
             </div>
         </div>
     </div>
